35849bfaff807a81849c63033ce6ee58ffc3c2cd
tricky <trickya23@gmail.com>
2024-04-06 08:39:21 +0000

    add ogimage and twitter image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { getCldOgImageUrl } from "next-cloudinary";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
@@ -6,9 +7,44 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const ogImageUrl = getCldOgImageUrl({
+  src: "logo_jch8fh",
+  crop: "thumb",
+  format: "jpg",
+});
+
+const twitterImageUrl = getCldOgImageUrl({
+  src: "logo_jch8fh",
+  crop: "thumb",
+  format: "webp",
+});
+
 export const metadata: Metadata = {
   title: "MPK Cargo Logistics",
   description: "Your total logistics partner for importing from China",
+  openGraph: {
+    images: [
+      {
+        url: ogImageUrl,
+        width: 1200,
+        height: 630,
+        alt: "MPK Cargo Logistics",
+      },
+    ],
+  },
+  twitter: {
+    site: "@mpk_cargo",
+    title: "MPK Cargo Logistics",
+    description: "Your total logistics partner for importing from China",
+    images: [
+      {
+        url: twitterImageUrl,
+        width: 1200,
+        height: 630,
+        alt: "MPK Cargo Logistics",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
